ecf40f02ccaa1cb9b5cf286012088d5d57cd9f96
txtyb <txtybbill@163.com>
2018-08-08 09:34:49 +0800

    增加了对文件名中空格的规避处理

--- flv2mp4.py
+++ flv2mp4.py
@@ -4,7 +4,7 @@ flv=list()
 for filename in os.listdir():
     #i=os.path.splitext(filename)
     if os.path.splitext(filename)[1] not in {'.mp4','.mp3','.py'}:
-        flv.append(filename)
+        flv.append(filename.replace(' ','\ '))
 print(flv)
 for i in flv:
-    os.system('ffmpeg -i '+i+' '+'-acodec copy -vcodec copy '+os.path.splitext(i)+'.mp4')
+    os.system('ffmpeg -i '+i+' '+'-acodec copy -vcodec copy '+os.path.splitext(i)[0]+'.mp4')
