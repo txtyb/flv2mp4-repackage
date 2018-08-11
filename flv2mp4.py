7a810af155f6dbafae4e19a48e828c084055917f
txtyb <txtybbill@163.com>
2018-08-11 21:43:08 +0800

    增加了多文件名中'&'的规避处理

--- flv2mp4.py
+++ flv2mp4.py
@@ -4,7 +4,7 @@ flv=list()
 for filename in os.listdir():
     #i=os.path.splitext(filename)
     if os.path.splitext(filename)[1] not in {'.mp4','.mp3','.py'}:
-        flv.append(filename.replace(' ','\ '))
+        flv.append(filename.replace(' ','\ ').replace('&','\&'))
 print(flv)
 for i in flv:
     os.system('ffmpeg -i '+i+' '+'-acodec copy -vcodec copy '+os.path.splitext(i)[0]+'.mp4')
